9b9ec1c979499553638e7164c7965e5c744c7cc8
Julien Kilo <julien.kilo@link-value.fr>
2017-11-25 15:11:07 +0000

    add micro-cors to handle CORS, fix deploy on now

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import micro from 'micro';
 import graphqlHTTP from 'express-graphql';
 import Schema from './schema/schema.js';
 
-exports.default = graphqlHTTP({ schema: Schema, pretty: true, graphiql: true });
+const CORS = require('micro-cors')();
+
+exports.default = CORS(graphqlHTTP({ schema: Schema, pretty: true, graphiql: true }));
